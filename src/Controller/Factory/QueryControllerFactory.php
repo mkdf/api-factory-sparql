5d146348ac07c3939d92287df26110ae583d266e
Jason Carvalho <jase@jasoncarvalho.com>
2021-06-08 12:58:59 +0100

    Add SPARQLQueryType service class

--- src/Controller/Factory/QueryControllerFactory.php
+++ src/Controller/Factory/QueryControllerFactory.php
@@ -5,6 +5,7 @@ namespace APIF\Sparql\Controller\Factory;
 use APIF\Sparql\Controller\QueryController;
 use APIF\Sparql\Repository\GraphRepositoryInterface;
 use APIF\Core\Repository\APIFCoreRepositoryInterface;
+use APIF\Sparql\Service\SPARQLQueryTypeInterface;
 use Interop\Container\ContainerInterface;
 use Laminas\ServiceManager\Factory\FactoryInterface;
 
@@ -19,6 +20,7 @@ class QueryControllerFactory implements FactoryInterface
         $config = $container->get("Config");
         $repository = $container->get(GraphRepositoryInterface::class);
         $apif_repository = $container->get(APIFCoreRepositoryInterface::class);
+        //$sparqlTypeChecker = $container->get(SPARQLQueryTypeInterface::class);
         return new QueryController($repository,$apif_repository,$config);
     }
 }
